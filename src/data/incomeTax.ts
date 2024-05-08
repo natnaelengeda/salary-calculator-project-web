7a7898a47feb314784dfbafcc77045e21f0d2962
nattyengeda <nattynengeda@gmail.com>
2024-05-08 16:25:56 +0300

    chore: Remove unused income tax data

--- src/data/incomeTax.ts
+++
@@ -1,9 +0,0 @@
-export const taxInfo = [
-  { id: 1, min: 0, max: 600, rate: 0, dec: 0 },
-  { id: 2, min: 601, max: 1650, rate: 10, dec: 60 },
-  { id: 3, min: 1651, max: 3200, rate: 15, dec: 142.50 },
-  { id: 4, min: 3201, max: 5250, rate: 20, dec: 302.50 },
-  { id: 5, min: 5251, max: 7800, rate: 25, dec: 565 },
-  { id: 6, min: 7801, max: 10900, rate: 30, dec: 955 },
-  { id: 7, min: 10900, max: 10900, rate: 35, dec: 1500 },
-]
